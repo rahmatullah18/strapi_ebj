c87665a1884447a8b44c503aa4c47d6983a7ac8a
unknown <rahmatullahsmakenza@gmail.com>
2022-08-17 20:41:48 +0800

    add menambahkan fuzzy search plugins

--- config/plugins.js
+++ config/plugins.js
@@ -20,5 +20,73 @@ module.exports = ({ env }) => ({
   // enable Awesome Help
   "awesome-help": {
     enabled: true,
-  }
+  },
+
+  "fuzzy-search": {
+    enabled: true,
+    config: {
+      contentTypes: [
+        {
+          uid: "api::shop.shop",
+          modelName: "shop",
+          queryConstraints: {
+            where: {
+              $and: [
+                {
+                  publishedAt: { $notNull: true },
+                },
+              ],
+            },
+          },
+          fuzzysortOptions: {
+            characterLimit: 300,
+            threshold: -600,
+            limit: 10,
+            keys: [
+              {
+                name: "shop_name",
+                weight: 100,
+              },
+              {
+                name: "kecamatan",
+                weight: -100,
+              },
+              {
+                name: "shop_description",
+                weight: -100,
+              },
+            ],
+          },
+        },
+        {
+          uid: "api::category.category",
+          modelName: "category",
+          queryConstraints: {
+            where: {
+              $and: [
+                {
+                  publishedAt: { $notNull: true },
+                },
+              ],
+            },
+          },
+          fuzzysortOptions: {
+            characterLimit: 300,
+            threshold: -600,
+            limit: 10,
+            keys: [
+              {
+                name: "category_name",
+                weight: 100,
+              },
+              {
+                name: "description",
+                weight: 100,
+              },
+            ],
+          },
+        },
+      ],
+    },
+  },
 });
